a258a85478fe8ad7d13d573f4e5729c80f30ac24
aline.bezzoco <aline.bezzoco@stilingue.com.br>
2022-06-27 11:03:18 -0300

    feat: create header component

---
+++ tests/unit/Header.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+import Header from "../../src/common/components/Header/Header.vue";
+
+describe("Header.vue", () => {
+  it("renders title props when passed", () => {
+    const title = "Extrato";
+    const wrapper = shallowMount(Header, {
+      props: { title },
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
+  
+  it("should doesn't find any title", () => {
+    const title = "";
+    const wrapper = shallowMount(Header, {
+      props: { title },
+    });
+    expect(wrapper.text()).toBe("");
+  });
+});
