2fc62610773377457ec1b09ab38ca07ade111061
aline.bezzoco <aline.bezzoco@stilingue.com.br>
2022-06-27 12:51:12 -0300

    feat: create filter search component

---
+++ src/common/components/Search/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchComponent",
+  props: {
+    value: String,
+  },
+};
+</script>
