1409edc5ed10869b6bbd89e64140f83abf236993
aline.bezzoco <aline.bezzoco@stilingue.com.br>
2022-06-27 12:50:45 -0300

    feat: create table component

---
+++ src/common/components/Table/Table.vue
@@ -0,0 +1,23 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="field in fields" :key='field'>{{ field }}</th>
+            </tr>
+        </thead>
+        <slot>
+        </slot>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'TableComponent',
+    props: {
+        fields: Array
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/common/components/Table/TableBody.vue
@@ -0,0 +1,39 @@
+<template>
+    <tbody v-bind="$attrs">
+        <slot v-for="(result, index) in items" :key="index">
+            <td>
+            <th>{{ $dateFormat.formatDateDMY(result.date) }}</th>
+            </td>
+            <tr v-for="(item, i) in result.items" :key="i">
+                <td>{{ item.actor }}</td>
+                <td>{{ item.status }}</td>
+                <td>{{ $dateFormat.formatDateDMH(item.dateEvent) }}</td>
+                <td>{{ $currencyFormat.convertToBRL(item.amount) }}</td>
+            </tr>
+            <td>
+            <th>Saldo do dia: {{ $currencyFormat.convertToBRL(result.amountTotal) }}</th>
+            </td>
+        </slot>
+    </tbody>
+    <tbody v-bind="$attrs">
+        <tr v-for="(item, index) in filteredItems" :key="index">
+            <td>{{ item.actor }}</td>
+            <td>{{ item.status }}</td>
+            <td>{{ $dateFormat.formatDateDMH(item.dateEvent) }}</td>
+            <td>{{ $currencyFormat.convertToBRL(item.amount) }}</td>
+        </tr>
+    </tbody>
+</template>
+
+<script>
+export default {
+    name: 'TableBodyComponent',
+    props: {
+        items: Array,
+        filteredItems: Array,
+    }
+}
+</script>
+
+<style>
+</style>
