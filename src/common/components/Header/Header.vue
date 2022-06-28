e158e6ad9ff25aa1a31e48af00c71cc1b88ab0b3
aline.bezzoco <aline.bezzoco@stilingue.com.br>
2022-06-28 10:37:22 -0300

    refactor: text alignment

--- src/common/components/Header/Header.vue
+++ src/common/components/Header/Header.vue
@@ -16,22 +16,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-header { 
+header {
     border-bottom: 1px solid var(--gray-200);
     margin-bottom: 56px;
 }
-.title { 
-    width: 50%;
-    padding-left: 50px;
-    @media(max-width: 780px) { 
+
+.title {
+    width: 60%;
+    margin: 0 auto;
+
+    @media(max-width: 780px) {
         width: 100%;
-        padding-left: 0;
     }
 }
+
 h2 {
     font-weight: 900;
     font-size: 22px;
     line-height: 32px;
+    text-align: left;
     color: var(--gray-500);
 
 }
